7853c1969d1e9cf13b0996e43632fa86f246a403
SonHanSol1 <gksthf7869@naver.com>
2023-03-17 17:14:41 +0900

    피자 나눠 먹기(1)

---
+++ src/main/java/com/ll/level0/p120814/Main.java
@@ -0,0 +1,27 @@
+package com.ll.level0.p120814;
+//https://school.programmers.co.kr/learn/courses/30/lessons/120814
+//피자 나눠 먹기(1)
+public class Main {
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n > 7) {
+            answer = n/7;
+            if(n%7 != 0) {
+                answer += 1;
+            }
+            return answer;
+        }
+        return 1;
+    }
+}
+
+/*
+class Solution {
+    public int solution(int n) {
+        return (n + 6) / 7;
+    }
+}
+ */
