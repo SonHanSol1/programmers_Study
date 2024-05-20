7cc5c440e0a16a51b86568f995e5e9967c69c080
SonHanSol1 <gksthf7869@naver.com>
2024-05-20 16:50:40 +0900

    fix : 불필요한 부분 삭제 및 수정

--- src/main/java/com/ll/level0/p120861/Main.java
+++ src/main/java/com/ll/level0/p120861/Main.java
@@ -40,21 +40,18 @@ import java.util.HashMap;
 //        return answer;
 //    }
 //}
-////class Solution {
-////    public int[] solution(String[] keyinput, int[] board) {
-////        int[] answer = {0,0};
-////        String[] str = {"up", "down", "right", "left"};
-////        HashMap<String, Integer> key = new HashMap<>();
-////        key.put("up", 1);
-////        key.put("down", -1);
-////        key.put("right", 1);
-////        key.put("left", -1);
-////        for(String key : keyinput){
-////            if(key)
-////        }
-////        return answer;
-////    }
-////}
+//class Solution {
+//    public int[] solution(String[] keyinput, int[] board) {
+//        int[] now = {0, 0};
+//        for (int i = 0; i < keyinput.length; i++){
+//            if(keyinput[i].equals("left")) now[0] -= now[0]>-(board[0]/2)?1:0;
+//            else if(keyinput[i].equals("right")) now[0] += now[0]<(board[0]/2)?1:0;
+//            else if(keyinput[i].equals("down")) now[1] -= now[1]>-(board[1]/2)?1:0;
+//            else if(keyinput[i].equals("up")) now[1] += now[1]<(board[1]/2)?1:0;
+//        }
+//        return now;
+//    }
+//}
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
         int[] answer = {0, 0};
